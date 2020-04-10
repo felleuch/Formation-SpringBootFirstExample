a5ee1975f02fde15f485e73b359bdc79e86f3a54
ELLEUCH <ELLEUCH@DESKTOP-E28062K>
2020-04-10 11:00:15 +0200

    init project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+	
+	@GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Spring Boot  premier exemple");
+        model.addAttribute("msg", "Welcome to Spring boot app !");
+        return "index";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	
+	}
+
+}
